b67a340c545970315e5c471165ab3b86160cf965
gaurav2132 <gaurav2132@gmail.com>
2012-09-25 21:56:52 -0500

    First Week

---
+++ TestProcessing/src/cs422/test/Map.java
@@ -0,0 +1,63 @@
+package cs422.test;
+
+
+import org.gicentre.geomap.GeoMap;
+
+import processing.core.PApplet;
+
+
+public class Map extends PApplet{
+	/*
+	  Draws a simple interactive world map that you can query.
+	  It uses the giCentre's geoMap library.
+	  Iain Dillingham, 20th January 2011.
+	*/
+	
+	public static void main(String args[]) {
+	    PApplet.main(new String[] { "--present", "cs422.test.GeoMap" });
+	  }
+	
+	GeoMap geoMap;
+	//GeoMap geoMap2;
+	public void setup()
+	{
+	  size(1000, 500);
+	  smooth();
+	  geoMap = new GeoMap(0, 0, width, height, this);
+	 // geoMap2 = new GeoMap(this);
+	  
+	 
+	  geoMap.readFile("../cntry08");
+	  //geoMap.getAttributes().writeAsTable(5);
+	//  geoMap.screenToGeo(width - 500, height - 100);
+	}
+
+	public void draw()
+	{
+		 background(180, 210, 240);
+		  stroke(0);
+		  fill(150, 190, 150);
+		  geoMap.draw();
+
+		  int id = geoMap.getID(mouseX, mouseY);
+		  if (id != -1)
+		  {
+		    fill(180, 120, 120);
+		    geoMap.draw(id);
+		  }
+
+		  noStroke();
+		  fill(255, 192);
+		  rect(0, 0, width, 20);
+
+		  if (id != -1)
+		  {
+		    String name = geoMap.getAttributes().getString(id, 6);
+		    fill(0);
+		    textAlign(LEFT, CENTER);
+		    text(name, 0, 0, width, 20);
+		  }
+
+	}
+
+}
